2df6351925fd5270f8997647f573e7c8d27c9696
Samuel Cha <sunchipssam@gmail.com>
2018-08-27 10:27:59 -0700

    Init commit

---
+++ src/main/java/com/example/cashenvelope/DemoApplication.java
@@ -0,0 +1,17 @@
+package com.example.cashenvelope;
+package demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.*;
+
+@Controller
+@SpringBootApplication
+public class DemoApplication {
+	
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+}
