27bb40d96ff8a3b5c847f4ead55de51178f9c912
Samuel Cha <sunchipssam@gmail.com>
2018-08-27 10:31:08 -0700

    Removed demo pkg

--- src/main/java/com/example/cashenvelope/DemoApplication.java
+++ src/main/java/com/example/cashenvelope/DemoApplication.java
@@ -1,5 +1,4 @@
 package com.example.cashenvelope;
-package demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,7 +8,12 @@ import org.springframework.stereotype.*;
 @Controller
 @SpringBootApplication
 public class DemoApplication {
-	
+	@RequestMapping("/")
+	@ResponseBody
+
+	String Home() {
+		return "Hello World!";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
