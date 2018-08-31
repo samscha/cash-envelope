c1ff2a92e6d28ede02cf199ea0163abc5b7d7785
Samuel Cha <sunchipssam@gmail.com>
2018-08-31 11:44:09 -0700

    Deleted launch json for now

--- src/test/java/com/example/cashenvelope/CashEnvelopeApplicationTests.java
+++ src/test/java/com/example/cashenvelope/CashEnvelopeApplicationTests.java
@@ -1,16 +1,16 @@
-package com.example.cashenvelope;
+// package com.example.cashenvelope;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+// import org.junit.Test;
+// import org.junit.runner.RunWith;
+// import org.springframework.boot.test.context.SpringBootTest;
+// import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
-public class CashEnvelopeApplicationTests {
+// @RunWith(SpringRunner.class)
+// @SpringBootTest
+// public class CashEnvelopeApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+// 	@Test
+// 	public void contextLoads() {
+// 	}
 
-}
+// }
