5b3983ec0e54688ad2595f48748d7cc2c0b5d09a
Samuel Cha <sunchipssam@gmail.com>
2018-09-06 13:09:35 -0700

    Deleted commented import

--- src/main/java/com/example/cashenvelope/user/UserController.java
+++ src/main/java/com/example/cashenvelope/user/UserController.java
@@ -15,7 +15,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-// import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -41,7 +40,7 @@ public class UserController {
     }).orElseThrow(() -> new UnprocessableEntityException("User not found"));
   }
 
-  @PostMapping("/user")
+  @PostMapping("/users")
   public User createUser(@Valid @RequestBody User user) {
     return userRepository.save(user);
   }
