795a91faea897d171be23cbab5d1031978bfe305
Samuel Cha <sunchipssam@gmail.com>
2018-11-22 12:50:37 -0600

    remove cors for now

--- src/main/java/com/example/cashenvelope/config/WebConfig.java
+++ src/main/java/com/example/cashenvelope/config/WebConfig.java
@@ -1,23 +1,24 @@
-package com.example.cashenvelope.config;
+// package com.example.cashenvelope.config;
 
-import java.util.ArrayList;
-import java.util.List;
+// import java.util.ArrayList;
+// import java.util.List;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.web.servlet.config.annotation.CorsRegistry;
+// import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+// import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
-@EnableWebMvc
-public class WebConfig implements WebMvcConfigurer {
+// @Configuration
+// @EnableWebMvc
+// public class WebConfig implements WebMvcConfigurer {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        List<String> origins = new ArrayList<String>();
+// @Override
+// public void addCorsMappings(CorsRegistry registry) {
+// List<String> origins = new ArrayList<String>();
 
-        origins.add(System.getenv("ORIGIN"));
+// origins.add(System.getenv("ORIGIN"));
 
-        registry.addMapping("/**").allowedOrigins(origins.toArray(new String[origins.size()])).allowCredentials(true);
-    }
-}
+// registry.addMapping("/**").allowedOrigins(origins.toArray(new
+// String[origins.size()])).allowCredentials(true);
+// }
+// }
